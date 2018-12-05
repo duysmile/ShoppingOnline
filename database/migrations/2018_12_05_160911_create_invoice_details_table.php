8cacadb982246db1cbe2cc3751e36ece3dd174d7
Duy Nguyen <duy210697@gmail.com>
2018-12-05 23:35:26 +0700

    ft #5 client: add migration invoices

---
+++ database/migrations/2018_12_05_160911_create_invoice_details_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvoiceDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoice_details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('invoice_id')->unsigned();
+            $table->integer('product_id')->unsigned();
+            $table->integer('quantity');
+            $table->integer('status')->unsigned();
+
+            $table->foreign('invoice_id')->references('id')->on('invoices')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoice_details');
+    }
+}
